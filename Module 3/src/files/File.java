1eb8c2fa6cea8a138c7687c104b61d8d4dd6177c
Ilya Rubinsky <ilya.rubinsky@gmail.com>
2015-12-28 15:31:10 +0200

    exception handling updated

--- Module 3/src/files/File.java
+++ Module 3/src/files/File.java
@@ -3,10 +3,16 @@ package files;
 public class File {
     String name;
     public File(String name) {
-        if (name.length()>20) try {
-            throw new IllegalFileNameException();
+        try {
+            this.setName(name);
         } catch (IllegalFileNameException e) {
             System.out.println("[Error: ]File name length too big!");
+        }
+    }
+
+    private void setName(String name) throws IllegalFileNameException {
+        if (name.length()>20) {
+            throw new IllegalFileNameException();
         }
         this.name=name;
     }
